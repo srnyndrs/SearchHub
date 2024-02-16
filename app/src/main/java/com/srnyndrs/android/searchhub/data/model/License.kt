d1edb7f7caa79578dabe2f9343c0949a95b212aa
Surányi András <srnyndrs@gmail.com>
2024-02-16 16:46:42 +0100

    Refactored data classes

--- app/src/main/java/com/srnyndrs/android/searchhub/data/model/License.kt
+++ app/src/main/java/com/srnyndrs/android/searchhub/data/model/License.kt
@@ -1,9 +1,16 @@
 package com.srnyndrs.android.searchhub.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class License (
     val key: String? = null,
     val name: String? = null,
+
+    @SerializedName("spdx_id")
     val spdxID: String? = null,
+
     val url: String? = null,
+
+    @SerializedName("node_id")
     val nodeID: String? = null
 )
